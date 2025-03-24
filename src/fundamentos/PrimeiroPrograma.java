bd1c3fe8ddaaa60ad2d0bae594665899c9fd10ae
gianverdum <gian.verdum@gmail.com>
2025-03-24 09:04:29 -0300

    Primeiro exercicio finalizado

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -1,8 +1,18 @@
 package fundamentos;
 
+/**
+ * @author gian.verdum <@gmail.com>
+ * @since JDK17
+ * @see "https://github.com/gianverdum/java-exercicios"
+ */
 public class PrimeiroPrograma {
 
+    /**
+     *
+     * @param args Array of command-line arguments passed to the program
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("I'm here!");
     }
 }
