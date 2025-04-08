7007670ca377f9dbe1e80b00c935f7cbe54b95db
gianverdum <gian.verdum@gmail.com>
2025-04-08 11:49:21 -0300

    Filter challenge

--- src/aulas/streams/Aluno.java
+++ src/aulas/streams/Aluno.java
@@ -4,9 +4,11 @@ public class Aluno {
 
     final String nome;
     final double nota;
+    final boolean behave;
 
-    public Aluno(String nome, double nota) {
+    public Aluno(String nome, double nota, boolean behave) {
         this.nome = nome;
         this.nota = nota;
+        this.behave = behave;
     }
 }
--- src/aulas/streams/Filter.java
+++ src/aulas/streams/Filter.java
@@ -9,21 +9,23 @@ public class Filter {
 
     public static void main(String[] args) {
 
-        Aluno aluno1 = new Aluno("Ana", 7.8);
-        Aluno aluno2 = new Aluno("Bia", 5.8);
-        Aluno aluno3 = new Aluno("Daniel", 9.8);
-        Aluno aluno4 = new Aluno("Gui", 6.8);
-        Aluno aluno5 = new Aluno("Rebeca", 7.1);
-        Aluno aluno6 = new Aluno("Pedro", 8.8);
+        Aluno aluno1 = new Aluno("Ana", 7.8, false);
+        Aluno aluno2 = new Aluno("Bia", 5.8, true);
+        Aluno aluno3 = new Aluno("Daniel", 9.8, true);
+        Aluno aluno4 = new Aluno("Gui", 6.8, true);
+        Aluno aluno5 = new Aluno("Rebeca", 7.1, true);
+        Aluno aluno6 = new Aluno("Pedro", 8.8, false);
 
         List<Aluno> alunos = Arrays.asList(aluno1, aluno2, aluno3, aluno4, aluno5, aluno6);
 
         Predicate<Aluno> aprovado = a -> a.nota >= 7.0;
+        Predicate<Aluno> comportado = a -> a.behave;
         Function<Aluno, String> saudacaoAprovado =
                 a -> "Parabéns " + a.nome + "! Você foi aprovado(a)!";
 
         alunos.stream()
                 .filter(aprovado)
+                .filter(comportado)
                 .map(saudacaoAprovado)
                 .forEach(System.out::println);
     }
