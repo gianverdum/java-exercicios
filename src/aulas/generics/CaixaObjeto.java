f1010ca000cb4bd8b575a0a832868dbb1414f6f2
gianverdum <gian.verdum@gmail.com>
2025-04-11 16:17:11 -0300

    Implementing the problem generics resolves

---
+++ src/aulas/generics/CaixaObjeto.java
@@ -0,0 +1,14 @@
+package aulas.generics;
+
+public class CaixaObjeto {
+
+    private Object coisa;
+
+    public void guardar(Object coisa) {
+        this.coisa = coisa;
+    }
+
+    public Object abrir() {
+        return coisa;
+    }
+}
