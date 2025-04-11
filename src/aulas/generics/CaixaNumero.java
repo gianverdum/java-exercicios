702f6041eed0962006f1a997943c379361b1707c
gianverdum <gian.verdum@gmail.com>
2025-04-11 16:39:06 -0300

    Using generics restricting numbers

---
+++ src/aulas/generics/CaixaNumero.java
@@ -0,0 +1,4 @@
+package aulas.generics;
+
+public class CaixaNumero<N extends Number> extends Caixa<N> {
+}
---
+++ src/aulas/generics/CaixaNumeroTeste.java
@@ -0,0 +1,15 @@
+package aulas.generics;
+
+public class CaixaNumeroTeste {
+
+    public static void main(String[] args) {
+
+        CaixaNumero<Double> caixaA = new CaixaNumero<>();
+        caixaA.guardar(3.1415);
+        System.out.println(caixaA.abrir());
+
+        CaixaNumero<Integer> caixaB = new CaixaNumero<>();
+        caixaB.guardar(3);
+        System.out.println(caixaB.abrir());
+    }
+}
