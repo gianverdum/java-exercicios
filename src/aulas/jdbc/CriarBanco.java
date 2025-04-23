b1691784cb19356b7948edeba9817622fe509926
gianverdum <gian.verdum@gmail.com>
2025-04-23 15:49:20 -0300

    Created connection factory class

--- src/aulas/jdbc/CriarBanco.java
+++ src/aulas/jdbc/CriarBanco.java
@@ -1,9 +1,6 @@
 package aulas.jdbc;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -11,14 +8,7 @@ public class CriarBanco {
 
     public static void main(String[] args) throws SQLException {
 
-        Dotenv dotenv = Dotenv.load();
-
-        String url = dotenv.get("URL");
-        String usuario = dotenv.get("USERNAME");
-        String senha = dotenv.get("PASSWORD");
-
-        Connection conexao = DriverManager
-                .getConnection(url,usuario,senha);
+        Connection conexao = FabricaConexao.getConnection();
 
         Statement stmt = conexao.createStatement();
         stmt.execute("CREATE DATABASE IF NOT EXISTS curso_java");
---
+++ src/aulas/jdbc/FabricaConexao.java
@@ -0,0 +1,25 @@
+package aulas.jdbc;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class FabricaConexao {
+
+    public static Connection getConnection() {
+        try {
+            Dotenv dotenv = Dotenv.load();
+
+            String url = dotenv.get("URL");
+            String usuario = dotenv.get("USERNAME");
+            String senha = dotenv.get("PASSWORD");
+
+            return DriverManager.getConnection(url,usuario,senha);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
