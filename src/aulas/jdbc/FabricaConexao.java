d9a9f226d133587835891a2b81da9e2ef76cfbb1
gianverdum <gian.verdum@gmail.com>
2025-04-24 11:35:33 -0300

    Replaced .env to .properties

--- src/aulas/jdbc/FabricaConexao.java
+++ src/aulas/jdbc/FabricaConexao.java
@@ -1,26 +1,33 @@
 package aulas.jdbc;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class FabricaConexao {
 
     public static Connection getConnection() {
         try {
-            Dotenv dotenv = Dotenv.load();
+            Properties properties = getProperties();
 
             String dbName = "curso_java";
-            String url = dotenv.get("URL") + dbName;
-            String usuario = dotenv.get("USERNAME");
-            String senha = dotenv.get("PASSWORD");
+            String url = properties.getProperty("URL") + dbName;
+            String usuario = properties.getProperty("USERNAME");
+            String senha = properties.getProperty("PASSWORD");
 
             return DriverManager.getConnection(url,usuario,senha);
 
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static Properties getProperties() throws IOException {
+        Properties properties = new Properties();
+        String caminho = "/conexao.properties";
+        properties.load(FabricaConexao.class.getResourceAsStream(caminho));
+        return properties;
     }
 }
